15017bd284c96d0e9e404f549641b1b505cc614c
k46416683 <72997497+k46416683@users.noreply.github.com>
2020-11-09 09:34:35 +0000

    fix factorial

--- lab4/example5.py
+++ lab4/example5.py
@@ -1,10 +1,8 @@
 n=int(input('enter number'))
 factorial=1
-if n>0:
+if n>=0:
   for i in range(1,n+1):
     factorial=factorial*i
   print(factorial)  
-elif n==0:
-  print(1)
 else:
   print('factorial can not be calculated because of negative number')      

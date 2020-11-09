9b03dd8f08c99bb2d48d7f2573926260f3183524
k46416683 <72997497+k46416683@users.noreply.github.com>
2020-11-09 09:26:37 +0000

    fix power example

--- lab4/example4.py
+++ lab4/example4.py
@@ -1,6 +1,11 @@
 a=int(input('write number'))
 b=int(input('write number'))
 c=1
-for i in range(b):
-  c=c*a
-print(c)  
+if b>0:
+  for i in range(b):
+    c=c*a
+  print(c)
+else:
+  for i in range(b):
+    c=c/a
+  print(c)      
